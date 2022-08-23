23b926edcc24965bf8e5a429300364559b61e840
Lucas Viana <lucasvfv@gmail.com>
2022-08-23 14:56:35 -0300

    fix: footer fixed

--- src/components/Historic/index.jsx
+++ src/components/Historic/index.jsx
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useContext, useState } from 'react';
 import styled from 'styled-components';
 import HistoricForm from './form.jsx';
