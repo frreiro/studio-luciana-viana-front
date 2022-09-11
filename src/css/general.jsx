a92a1f5354f44c6a425c0b172197dca0460f359d
Lucas Viana <lucasvfv@gmail.com>
2022-09-11 18:19:39 -0300

    feat: banner responsive

--- src/css/general.jsx
+++ src/css/general.jsx
@@ -1,5 +1,51 @@
 import styled from 'styled-components';
 
+
+export const Banner = styled.article`
+height: 409px;
+width: 50%;
+min-width: 243px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+position: relative;
+
+&.show-back{
+    transform: rotateY(180deg);
+}
+
+a{
+    font-family: var(--body-font);
+    font-weight: lighter;
+    font-size: 10px;
+    color: var(--button-color);
+    /*margin-bottom: 40px;*/
+    opacity: 0.7;
+}
+
+`;
+
+export const Title = styled.div`
+margin-top: ${(props) => props.marginTop ? props.marginTop : 38}px;
+font-family: var(--title-font);
+color: var(--third-color);
+font-size: 16px;
+display: flex;
+flex-direction: column;
+align-items: center;
+/* padding: 40px 0 5px 0; */
+
+hr{
+    width: 170px;
+    height: 2px;
+    background-color: var(--third-color);
+}
+
+`;
+
 export const Container = styled.div`
 width: 100vw;
 height: 100vh ;
@@ -35,59 +81,22 @@ transition: transform 0.8s;
 transform-style: preserve-3d;
 
 
-
 `;
 
 export const FlipCard = styled.div`
+height: 409px;
+width: 50%;
+min-width: 243px;
+display: flex;
 background-color: transparent;
+
+${Banner}{
+	width: 100%;
+
+}
 
 &.show-back ${CardInner}{
     transform: rotateY(180deg);
-}
-
-`;
-
-export const Banner = styled.article`
-height: 409px;
-width: 243px;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-align-items: center;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 10px;
-position: relative;
-
-&.show-back{
-    transform: rotateY(180deg);
-}
-
-a{
-    font-family: var(--body-font);
-    font-weight: lighter;
-    font-size: 10px;
-    color: var(--button-color);
-    /*margin-bottom: 40px;*/
-    opacity: 0.7;
-}
-
-
-`;
-
-export const Title = styled.div`
-margin-top: ${(props) => props.marginTop ? props.marginTop : 38}px;
-font-family: var(--title-font);
-color: var(--third-color);
-font-size: 16px;
-display: flex;
-flex-direction: column;
-align-items: center;
-/* padding: 40px 0 5px 0; */
-
-hr{
-    width: 170px;
-    height: 2px;
-    background-color: var(--third-color);
 }
 
 `;
